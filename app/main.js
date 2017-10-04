0dbae1c9a4d28d5e690dca7acbaf478ff0ba6c29
Janine Hempy <jhempy@gmail.com>
2017-10-04 12:07:40 -0400

    Pulling from API and saving URLs

--- app/main.js
+++ app/main.js
@@ -3,10 +3,28 @@ var demo = new Vue({
   el: '#main',
 
   data: {
-    layout: 'grid'
+    layout: 'grid',
+    cats: []
   },
 
-  methods: {
+  mounted: function() {
+
+    var self = this;
+
+    axios.get("http://thecatapi.com/api/images/get?format=xml&results_per_page=6")
+      .then(function (response) {
+
+        var parser = new DOMParser();
+        var data = parser.parseFromString(response.data, "text/xml");
+
+        Array.from(data.getElementsByTagName('url'))
+        .forEach(function(url) {
+          self.cats.push(url.innerHTML);
+        });
+        
+      });
+
+    console.log(self.cats);
 
   }
 
